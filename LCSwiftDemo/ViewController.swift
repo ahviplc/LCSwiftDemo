cdb34c67d314ea5e4715164822b76de186e9297a
theMacOfLC <ahviplc@themacoflcdemac.local>
2018-12-18 13:55:06 +0800

    Initial Commit

---
+++ LCSwiftDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LCSwiftDemo
+//
+//  Created by theMacOfLC on 2018/12/18.
+//  Copyright © 2018年 theMacOfLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
